989b6fca3f10032a991d0c9b7a0c989739ff87be
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-02-27 11:23:37 +0100

    Added prop type and updated test for Avatar

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -2,12 +2,31 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import Avatar from "./Avatar";
+import { FALLBACK_AVATAR_ALT_TEXT, FALLBACK_AVATAR_URL } from "./Avatar";
 
 describe("Avatar", () => {
   it("should render an img with alt text", () => {
-    render(<Avatar />);
-    const img = screen.getByAltText("@github-handle");
+    const url = "https://cataas.com/cat/says/hello%20world!";
+    const alt = "a beautiful cat";
+    render(<Avatar url={url} alt={alt} />);
+    const img = screen.getByAltText(alt);
     expect(img).toBeInTheDocument();
     expect(img.tagName).toBe("IMG");
   });
+});
+
+it("should render the url and alt passed in", () => {
+  const url = "https://cataas.com/cat/says/hello%20world!";
+  const alt = "a beautiful cat";
+  render(<Avatar url={url} alt={alt} />);
+
+  const img = screen.getByAltText(alt);
+  expect(img).toHaveAttribute("src", url);
+});
+
+it("should use a fallback image if no URL passed in", () => {
+  render(<Avatar />);
+
+  const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+  expect(img).toHaveAttribute("src", FALLBACK_AVATAR_URL);
 });
