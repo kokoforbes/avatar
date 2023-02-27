15274a9251837c7df4ace00a09bc4748b78ca1b5
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-02-27 11:06:41 +0100

    Created Avatar component

---
+++ src/Avatar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <img
+      src='https://cataas.com/cat/says/hello%20world!'
+      alt='@github-handle'
+    />
+  );
+};
+
+export default Avatar;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTest.ts",
+    // https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: false,
+  },
+});
